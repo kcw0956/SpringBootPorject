fa88af57b97098aa6bc2e4fd73fcf8dd6a97d94a
kcw0956 <103244652+kcw0956@users.noreply.github.com>
2023-04-29 00:00:48 +0900

    일단 돌아가게 만든 버전 오류가 아쉽

--- springEx/src/main/java/com/co/kr/mapper/BookUploadMapper.java
+++ springEx/src/main/java/com/co/kr/mapper/BookUploadMapper.java
@@ -1,42 +1,42 @@
-package com.co.kr.mapper;
-
-import java.util.HashMap;
-import java.util.List;
-
-import org.apache.ibatis.annotations.Mapper;
-
-import com.co.kr.domain.BoardContentDomain;
-import com.co.kr.domain.BoardFileDomain;
-import com.co.kr.domain.BoardListDomain;
-import com.co.kr.domain.BookContentDomain;
-import com.co.kr.domain.BookFileDomain;
-import com.co.kr.domain.BookListDomain;
-
-
-@Mapper
-public interface BookUploadMapper {
-
-	// 전체 리스트 조회
-		public List<BookListDomain> boardList();
-		//content insert
-		public void contentUpload(BookContentDomain boardContentDomain);
-		//file insert
-		public void fileUpload(BookFileDomain boardFileDomain);
-
-		//content update
-		public void bdContentUpdate(BookContentDomain boardContentDomain);
-		//file updata
-		public void bdFileUpdate(BookFileDomain boardFileDomain);
-
-	  //content delete 
-		public void bdContentRemove(HashMap<String, Object> map);
-		//file delete 
-		public void bdFileRemove(BookFileDomain boardFileDomain);
-		x
-		//select one
-		public BookListDomain boardSelectOne(HashMap<String, Object> map);
-
-		//select one file
-		public List<BookFileDomain> boardSelectOneFile(HashMap<String, Object> map);
-
-}
+//package com.co.kr.mapper;
+//
+//import java.util.HashMap;
+//import java.util.List;
+//
+//import org.apache.ibatis.annotations.Mapper;
+//
+//import com.co.kr.domain.BoardContentDomain;
+//import com.co.kr.domain.BoardFileDomain;
+//import com.co.kr.domain.BoardListDomain;
+//import com.co.kr.domain.BookContentDomain;
+//import com.co.kr.domain.BookFileDomain;
+//import com.co.kr.domain.BookListDomain;
+//
+//
+//@Mapper
+//public interface BookUploadMapper {
+//
+//	// 전체 리스트 조회
+//		public List<BookListDomain> boardList();
+//		//content insert
+//		public void contentUpload(BookContentDomain boardContentDomain);
+//		//file insert
+//		public void fileUpload(BookFileDomain boardFileDomain);
+//
+//		//content update
+//		public void bdContentUpdate(BookContentDomain boardContentDomain);
+//		//file updata
+//		public void bdFileUpdate(BookFileDomain boardFileDomain);
+//
+//	  //content delete 
+//		public void bdContentRemove(HashMap<String, Object> map);
+//		//file delete 
+//		public void bdFileRemove(BookFileDomain boardFileDomain);
+//		
+//		//select one
+//		public BookListDomain boardSelectOne(HashMap<String, Object> map);
+//
+//		//select one file
+//		public List<BookFileDomain> boardSelectOneFile(HashMap<String, Object> map);
+//
+//}
